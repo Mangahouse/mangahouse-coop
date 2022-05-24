fe84d75956e0e0e5fa0efba80810cb8dfc86d369
makuru93 <m.f0930f.m@gmail.com>
2022-05-25 04:46:22 +0900

    test

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Layout from '../components/layout'
-import Main from '../components/Main'
+import Main from '../components/main'
 import { graphql } from 'gatsby'
 
 export default function Home() {
